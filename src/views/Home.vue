46e1d45864d81702658f5d15b782b7668570e2fe
Azhar Ahmed A <azhar.a.ahmed98@gmail.com>
2021-06-20 21:12:07 +0530

    Learning Basics of Vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
-  <hello-world />
+  <div class="home">
+    <h1>Home page</h1>
+    <v-btn dark>
+      Click Me!
+    </v-btn>
+    <v-btn class="pink accent-2 indigo--text text--lighten-5">
+      Hey There!
+    </v-btn>
+    <v-btn color="pink" text>
+      <span>Like!</span>
+      <v-icon small right>mdi-heart</v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {};
 </script>
