c52a91a5fb3fe3a105a7f258e01e9020618a011b
Azhar Ahmed A <azhar.a.ahmed98@gmail.com>
2021-08-11 10:43:49 +0530

    creating the dashboard component using dummy data.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,22 +2,26 @@
   <div class="dashboard">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-row dense>
-        <v-col cols="12" md="6">
-          <v-btn block color="primary" outlined>Hey!!</v-btn>          
-        </v-col>
-        <v-col cols="12" md="6">
-          <v-btn block color="primary" outlined>Hey!!</v-btn>          
-        </v-col>
-      </v-row>
-      <v-row justify-md="space-around">
-        <v-col cols="4" md="3">
-          <v-btn block color="primary" outlined>Hey!!</v-btn>          
-        </v-col>
-        <v-col cols="4" md="3">
-          <v-btn block color="primary" outlined>Hey!!</v-btn>          
-        </v-col>
-      </v-row>
+      <v-card v-for="(project, index) in projects" :key="index">
+        <v-row dense :class="`pa-3 project ${project.status}`">
+          <v-col cols="12" md="6">
+            <div class="caption grey--text">Project Title</div>
+            <div>{{ project.title }}</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Due Date</div>
+            <div>{{ project.due }}</div>
+          </v-col>
+          <v-col cols="3" sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>{{ project.status }}</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -25,8 +29,29 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'Azhar Ahmed A', due: '1st Jan 2022', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Narendar Singh', due: '10th Jan 2021', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Parvez', due: '20th Dec 2021', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Adnan Zaid', due: '20th Oct 2020', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    };
   },
   methods: {},
 };
 </script>
+
+<style scoped>
+.project.complete {
+  border-left: 4px solid lightgreen;
+}
+
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+</style>
