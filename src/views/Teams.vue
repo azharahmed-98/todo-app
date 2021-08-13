a41d33b947c29562448d25fb6fa5892a77250a61
Azhar Ahmed A <azhar.a,ahmed98@gmail.com>
2021-08-14 00:08:38 +0530

    Adding the Team Member cards

--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="teams">
-    <h1 class="subtitle-1 grey--text">Team</h1>
     <v-container class="my-5">
-      content
+      <h1 class="subtitle-1 grey--text my-5">Team</h1>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+          v-for="(person, index) in teams"
+          :key="index"
+        >
+          <v-card class="text-center">
+            <v-responsive class="pt-4">
+              <v-avatar>
+                
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" text>
+                <v-icon small left>
+                  mdi-message
+                </v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
 export default {
-    
-}
+  data() {
+    return {
+      teams: [
+        { name: "Azhar Ahmed A", role: "Web Developer" },
+        { name: "Narendar Singh", role: "Digital Marketer" },
+        { name: "Adnan Zaid", role: "Graphics Designer" },
+        { name: "Parvez", role: "UX Developer" },
+      ],
+    };
+  },
+};
 </script>
